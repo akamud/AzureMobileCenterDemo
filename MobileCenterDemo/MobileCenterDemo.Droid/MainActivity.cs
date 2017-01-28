a28e49e478ee2840dd9fee43b30074dbb211986b
Mahmoud Ali <mahmoud.ali@lambda3.com.br>
2017-01-28 16:04:08 -0200

    Add Azure Mobile Center SDK

--- MobileCenterDemo/MobileCenterDemo.Droid/MainActivity.cs
+++ MobileCenterDemo/MobileCenterDemo.Droid/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Microsoft.Azure.Mobile;
 
 namespace MobileCenterDemo.Droid
 {
@@ -20,6 +21,9 @@ namespace MobileCenterDemo.Droid
             base.OnCreate(bundle);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
+
+            MobileCenter.Configure("mykey");
+
             LoadApplication(new App());
         }
     }
--- MobileCenterDemo/MobileCenterDemo/App.xaml.cs
+++ MobileCenterDemo/MobileCenterDemo/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +15,8 @@ namespace MobileCenterDemo
         public App()
         {
             InitializeComponent();
+
+            MobileCenter.Start(typeof(Analytics), typeof(Crashes));
 
             MainPage = new MobileCenterDemo.MainPage();
         }
